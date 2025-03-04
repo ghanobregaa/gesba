17ace266a9cd9baf64f39df7638d2046d46b9355
Guilherme Nóbrega <ghanobrega@gmail.com>
2025-03-04 02:43:00 +0000

    update

--- src/components/navbar/Navbar.jsx
+++ src/components/navbar/Navbar.jsx
@@ -1,22 +1,95 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import styles from "./Navbar.module.css";
-
+import { HiBars3, HiMiniBars3, HiMiniXMark } from "react-icons/hi2";
 export default function Navbar() {
-  return (
-    <div className={styles.navbar}>
-      <div className={styles.bkg}>
+  const [climate, setClimate] = useState([]);
+  const [open, setOpen] = useState(false);
+  const [topo, setTopo] = useState(true);
+  const [scrollDirection, setScrollDirection] = useState(true);
+  useEffect(() => {
+    getData();
+  }, []);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY < 500) {
+        setTopo(true);
+      } else {
+        setTopo(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+  useEffect(() => {
+    let lastScrollY = window.scrollY;
+
+    const updateScrollDirection = () => {
+      const currentScrollY = window.scrollY;
+      if (currentScrollY < lastScrollY) {
+        setScrollDirection(true); // Scrolling Up
+      } else {
+        setScrollDirection(false); // Scrolling Down
+      }
+      lastScrollY = currentScrollY;
+    };
+
+    window.addEventListener("scroll", updateScrollDirection);
+    return () => window.removeEventListener("scroll", updateScrollDirection);
+  }, []);
+
+  async function getData() {
+    try {
+      fetch(
+        "https://api.ipma.pt/open-data/forecast/meteorology/cities/daily/2310300.json"
+      )
+        .then((res) => res.json())
+        .then((data) => setClimate(data));
+    } catch {}
+  }
+
+  return scrollDirection || open ? (
+    <div
+      className={`${styles.navbar} ${open ? styles.open : ""} ${
+        !topo ? styles.topo : ""
+      } ${scrollDirection && !open ? styles.showP : ""}`}
+    >
+      <div className={styles.container}>
         <div className={styles.logo}>
-          <img src="logo.svg" />
+          {open || !topo ? (
+            <img src="logo_black.svg" />
+          ) : (
+            <img src="logo.svg" />
+          )}
         </div>
-        <div className={styles.row}>
+        <div
+          className={styles.hamburguer}
+          onClick={() => {
+            setOpen(!open);
+          }}
+        >
+          {open ? <HiMiniXMark size={45} /> : <HiMiniBars3 size={35} />}
+        </div>
+
+        <div className={styles.rowContent}>
           <a>Sobre nós</a>
-          <a>Comunicados</a>
           <a>Produção</a>
           <a>Produtores</a>
           <a>Documentação</a>
           <a>Contactos</a>
         </div>
       </div>
+      <div className={styles.rowContentHamburguer}>
+        <a>Sobre nós</a>
+        <a>Produção</a>
+        <a>Produtores</a>
+        <a>Documentação</a>
+        <a>Contactos</a>
+      </div>
     </div>
+  ) : (
+    ""
   );
 }
--- src/main.jsx
+++ src/main.jsx
@@ -5,9 +5,7 @@ import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 
 createRoot(document.getElementById("root")).render(
-  <StrictMode>
-    <BrowserRouter basename="/gesba/">
-      <App />
-    </BrowserRouter>
-  </StrictMode>
+  <BrowserRouter basename="/gesba/">
+    <App />
+  </BrowserRouter>
 );
