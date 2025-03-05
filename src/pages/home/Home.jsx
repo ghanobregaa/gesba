41d215202e28d830560a9084e5ac039881a5391f
Guilherme Nobrega <ghanobrega@gmail.com>
2025-03-05 17:46:53 +0000

    update

--- src/pages/home/Home.jsx
+++ src/pages/home/Home.jsx
@@ -1,8 +1,13 @@
 import CountUp from "react-countup";
 import Navbar from "../../components/navbar/Navbar";
 import styles from "./Home.module.css";
+import { useRef } from "react";
+import { useIsVisible } from "../../components/viewer/viewer";
+import Footer from "../../components/footer/Footer";
 
 export default function Home() {
+  const ref = useRef();
+  const isVisible = useIsVisible(ref);
   return (
     <>
       <header>
@@ -19,22 +24,32 @@ export default function Home() {
       </section>
       <section className={styles.section2}>
         <div className={styles.container}>
-          <div className={styles.rowCount}>
-            <div className={styles.columnN}>
-              <div className={styles.number}>
-                <CountUp end={2900} separator=" " />
+          <div ref={ref} className={`${styles.rowCount}`}>
+            {isVisible ? (
+              <div className={styles.columnN}>
+                <div className={styles.number}>
+                  <CountUp end={2900} separator=" " />
+                </div>
+                <div className={styles.subText}>Produtores</div>
               </div>
-              <div className={styles.subText}>Produtores</div>
-            </div>
-            <div className={styles.columnN}>
-              <div className={styles.number}>
-                <CountUp end={150101} separator=" " suffix=" kg" />
+            ) : (
+              ""
+            )}
+            {isVisible ? (
+              <div className={styles.columnN}>
+                <div className={styles.number}>
+                  <CountUp end={150101} separator=" " suffix=" kg" />
+                </div>
+                <div className={styles.subText}>Produção março</div>
               </div>
-              <div className={styles.subText}>Produção março</div>
-            </div>
+            ) : (
+              ""
+            )}
           </div>
         </div>
       </section>
+      {/* <section className={styles.section3}>a</section> */}
+      <Footer />
     </>
   );
 }
